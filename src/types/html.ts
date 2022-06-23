a6fef53f869a3627f175b8136df41881da892004
Diego Aquino <diegocruzdeaquino@gmail.com>
2022-06-23 18:22:57 -0300

    feat: add adottami logo

---
+++ src/types/html.ts
@@ -0,0 +1,5 @@
+import { ButtonHTMLAttributes, HTMLAttributes, SVGAttributes } from 'react';
+
+export type HTMLDivProps = HTMLAttributes<HTMLDivElement>;
+export type HTMLButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+export type SVGSVGProps = SVGAttributes<SVGSVGElement>;
