b79233e8e8c45ab5d157b0cb6684b99fd68edf24
Diego Aquino <diegocruzdeaquino@gmail.com>
2022-06-22 21:17:08 -0300

    feat: configure msw

---
+++ tests/test-server.ts
@@ -0,0 +1,5 @@
+import { setupServer } from 'msw/node';
+
+const testServer = setupServer();
+
+export default testServer;
--- tests/test-setup.ts
+++ tests/test-setup.ts
@@ -1,2 +1,15 @@
+import testServer from './test-server';
 import '@testing-library/jest-dom';
 import '@testing-library/jest-dom/extend-expect';
+
+beforeAll(async () => {
+  testServer.listen();
+});
+
+beforeEach(() => {
+  testServer.resetHandlers();
+});
+
+afterAll(() => {
+  testServer.close();
+});
