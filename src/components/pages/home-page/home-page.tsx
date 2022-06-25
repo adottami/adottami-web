119f43f92ed568cc3d964694dee83f5d04b8a7c3
Diego Aquino <diegocruzdeaquino@gmail.com>
2022-06-25 09:12:41 -0300

    feat: configure design system styles

--- src/components/pages/home-page/home-page.tsx
+++ src/components/pages/home-page/home-page.tsx
@@ -3,8 +3,8 @@ import { FC } from 'react';
 import { WELCOME_MESSAGE } from './constants';
 
 const HomePage: FC = () => (
-  <div className="flex h-screen w-screen items-center justify-center bg-blue-50">
-    <h1 className="text-4xl font-medium">{WELCOME_MESSAGE}</h1>
+  <div className="flex h-screen w-screen items-center justify-center bg-surface-primary">
+    <h1 className="text-4xl font-bold text-primary-dark">{WELCOME_MESSAGE}</h1>
   </div>
 );
 
