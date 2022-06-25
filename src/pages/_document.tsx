04089bb767ef4ee72ebc615ec54f7deb1398fd02
Diego Aquino <diegocruzdeaquino@gmail.com>
2022-06-25 08:56:07 -0300

    feat: configure default font family

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,10 @@ const Document: FC = () => (
       <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
       <link rel="apple-touch-icon" href="/images/apple-touch-icon.png" />
       <link rel="manifest" href="/manifest.webmanifest" />
+
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;700&display=swap" />
     </Head>
 
     <body>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
@@ -9,7 +11,9 @@ module.exports = {
 
       colors: {},
 
-      fontFamily: {},
+      fontFamily: {
+        sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+      },
 
       zIndex: {
         '-1': -1,
