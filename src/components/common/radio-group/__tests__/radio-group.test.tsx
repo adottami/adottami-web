700b9ae4ad46a1ee9183c5bf4b901ec54d081ede
Lucas Araújo de Lima <lucas.araujo.lima@ccc.ufcg.edu.br>
2022-07-01 17:51:45 -0300

    test: add-radio-group-test (#10)

--- src/components/common/radio-group/__tests__/radio-group.test.tsx
+++ src/components/common/radio-group/__tests__/radio-group.test.tsx
@@ -1,10 +1,36 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import RadioGroup from '../radio-group';
 
-describe('Radio group', () => {
-  it('should render correctly', () => {
-    render(<RadioGroup />);
-    // ...
+describe('Radio Group', () => {
+  it('should have radio input disabled correctly', () => {
+    render(
+      <RadioGroup
+        id="sexo"
+        label="Sexo"
+        options={[
+          { id: 'macho', label: 'Macho', isDisabled: true },
+          { id: 'femea', label: 'Fêmea' },
+        ]}
+      />,
+    );
+    const radio = screen.getByLabelText('Macho');
+    expect(radio).toBeDisabled();
+  });
+
+  it('should render list options', () => {
+    const { getByText } = render(
+      <RadioGroup
+        id="sexo"
+        label="Sexo"
+        options={[
+          { id: 'macho', label: 'Macho', isDisabled: true },
+          { id: 'femea', label: 'Fêmea' },
+        ]}
+      />,
+    );
+
+    expect(getByText('Macho')).toBeInTheDocument();
+    expect(getByText('Fêmea')).toBeInTheDocument();
   });
 });
