7e206e2939646e452617d9c25fa6be625a1798fc
Diego Aquino <diegocruzdeaquino@gmail.com>
2022-07-01 19:02:43 -0300

    test: verify `useAPI` and `useSession`

--- src/hooks/api/use-api/__tests__/use-api.test.ts
+++ src/hooks/api/use-api/__tests__/use-api.test.ts
@@ -1,10 +1,17 @@
+import APIContext from '@/contexts/api-context/api-context';
+import AdottamiClient from '@/services/adottami-client/adottami-client';
 import { renderHookWithTestProviders } from '@tests/utils/render';
 
 import useAPI from '../use-api';
 
 describe('useAPI', () => {
   it('should initialize correctly', () => {
-    renderHookWithTestProviders(useAPI);
-    // ...
+    const useAPIContextSpy = jest.spyOn(APIContext, 'useContext');
+
+    const { result } = renderHookWithTestProviders(useAPI);
+
+    expect(useAPIContextSpy).toHaveBeenCalled();
+    expect(result.current.adottami).toBeInstanceOf(AdottamiClient);
+    expect(result.current.setAdottami).toEqual(expect.any(Function));
   });
 });
