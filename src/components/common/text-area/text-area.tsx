8a002dfda6a765368bfea83a49b37dc8acc73060
felipe1496 <felipe.oliveira1496@gmail.com>
2022-07-02 17:58:16 -0300

    feat: textarea component created

--- src/components/common/text-area/text-area.tsx
+++ src/components/common/text-area/text-area.tsx
@@ -1,7 +1,14 @@
 import { FC } from 'react';
 
-interface Props {}
+interface Props extends React.HTMLProps<HTMLTextAreaElement> {}
 
-const TextArea: FC<Props> = () => null;
+const TextArea: FC<Props> = ({ ...rest }) => {
+  return (
+    <textarea
+      {...rest}
+      className="h-[7rem] w-full resize-none overflow-y-scroll rounded-pill border-2 border-neutral-100 bg-surface-primary p-4 text-md scrollbar-thin scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar-track-transparent scrollbar-thumb-secondary-medium focus:border-secondary-medium focus:outline-none focus:outline-none sm:h-[9.5rem]"
+    />
+  );
+};
 
 export default TextArea;
