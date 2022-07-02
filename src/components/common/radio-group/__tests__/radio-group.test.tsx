a392e9498886fc70220fa8d6771154a82e7a52ef
Lucas Araújo de Lima <lucas.araujo.lima@ccc.ufcg.edu.br>
2022-07-02 20:11:10 -0300

    Revert "test: add more tests (#10)"
    This reverts commit b2dd906ed4cfe843aaa2b3a44e6c3e020c86cae0.

--- src/components/common/radio-group/__tests__/radio-group.test.tsx
+++ src/components/common/radio-group/__tests__/radio-group.test.tsx
@@ -1,5 +1,4 @@
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 
 import RadioGroup from '../radio-group';
 
@@ -19,47 +18,19 @@ describe('Radio Group', () => {
     expect(radio).toBeDisabled();
   });
 
-  it('should render list options correctly', async () => {
+  it('should render list options', () => {
     const { getByText } = render(
       <RadioGroup
         id="sexo"
         label="Sexo"
         options={[
-          { id: 'macho', label: 'Macho' },
+          { id: 'macho', label: 'Macho', isDisabled: true },
           { id: 'femea', label: 'Fêmea' },
         ]}
       />,
     );
-
-    const options = screen.getAllByRole('radio');
 
     expect(getByText('Macho')).toBeInTheDocument();
     expect(getByText('Fêmea')).toBeInTheDocument();
-
-    expect(options).toHaveLength(2);
-  });
-
-  it('handles click correctly', async () => {
-    render(
-      <RadioGroup
-        id="sexo"
-        label="Sexo"
-        options={[
-          { id: 'macho', label: 'Macho' },
-          { id: 'femea', label: 'Fêmea' },
-        ]}
-      />,
-    );
-
-    const user = userEvent.setup();
-    await user.click(screen.getByText('Macho'));
-
-    expect(screen.getByLabelText('Macho')).toBeChecked();
-  });
-
-  it('should be required', async () => {
-    render(<RadioGroup id="sexo" label="Sexo" isRequired options={[]} />);
-
-    expect(screen.getByText('*')).toBeInTheDocument();
   });
 });
