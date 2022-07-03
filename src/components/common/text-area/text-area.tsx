662ad21bd3919d3d38a95ed3a10b4aeb5dc7991d
felipe1496 <felipe.oliveira1496@gmail.com>
2022-07-02 22:21:08 -0300

    feat: label added

--- src/components/common/text-area/text-area.tsx
+++ src/components/common/text-area/text-area.tsx
@@ -1,13 +1,23 @@
+import { Label } from '@radix-ui/react-label';
 import { FC } from 'react';
 
 interface Props extends React.HTMLProps<HTMLTextAreaElement> {}
 
-const TextArea: FC<Props> = ({ ...rest }) => {
+const TextArea: FC<Props> = ({ label, ...rest }) => {
   return (
-    <textarea
-      {...rest}
-      className="h-[7rem] w-full resize-none overflow-y-scroll rounded-pill border-2 border-neutral-100 bg-surface-primary p-4 text-md scrollbar-thin scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar-track-transparent scrollbar-thumb-secondary-medium focus:border-secondary-medium focus:outline-none focus:outline-none sm:h-[9.5rem]"
-    />
+    <div>
+      {label && (
+        <Label className="text-md font-bold text-primary-dark">
+          {label}
+          <div className="h-1" />
+        </Label>
+      )}
+
+      <textarea
+        {...rest}
+        className="h-[7rem] w-full resize-none overflow-y-scroll rounded-pill border-2 border-neutral-100 bg-surface-primary p-4 text-md text-primary-dark scrollbar-thin scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar-track-transparent scrollbar-thumb-secondary-medium focus:border-secondary-medium focus:outline-none sm:h-[9.5rem]"
+      />
+    </div>
   );
 };
 
