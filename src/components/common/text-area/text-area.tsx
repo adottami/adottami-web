0822a9446672b7dac0499df296a0c6ce018069f6
felipe1496 <felipe.oliveira1496@gmail.com>
2022-07-03 15:09:03 -0300

    feat: isRequired feat added

--- src/components/common/text-area/text-area.tsx
+++ src/components/common/text-area/text-area.tsx
@@ -1,21 +1,27 @@
 import { Label } from '@radix-ui/react-label';
 import { FC } from 'react';
 
-interface Props extends React.HTMLProps<HTMLTextAreaElement> {}
+interface Props extends React.HTMLProps<HTMLTextAreaElement> {
+  isRequired?: boolean;
+}
 
-const TextArea: FC<Props> = ({ label, id, ...rest }) => {
+const TextArea: FC<Props> = ({ label, isRequired, id, ...rest }) => {
   return (
     <div>
       {label && (
-        <Label htmlFor={id} className="text-md font-bold text-primary-dark">
-          {label}
+        <div>
+          <Label htmlFor={id} className="text-md font-bold text-primary-dark">
+            {label}
+            {isRequired && <span> *</span>}
+          </Label>
           <div className="h-1" />
-        </Label>
+        </div>
       )}
 
       <textarea
         {...rest}
         id={id}
+        required={isRequired}
         className="h-[7rem] w-full resize-none overflow-y-scroll rounded-pill border-2 border-neutral-100 bg-surface-primary p-4 text-md text-primary-dark scrollbar-thin scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar-track-transparent scrollbar-thumb-secondary-medium focus:border-secondary-medium focus:outline-none sm:h-[9.5rem]"
       />
     </div>
