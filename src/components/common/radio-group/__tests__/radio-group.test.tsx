df71016194ac9698e80483f7f571c0ac4bec40d6
Lucas Araújo de Lima <lucas.araujo.lima@ccc.ufcg.edu.br>
2022-07-03 11:23:49 -0300

    feat: create radio-group component (#10)

--- src/components/common/radio-group/__tests__/radio-group.test.tsx
+++ src/components/common/radio-group/__tests__/radio-group.test.tsx
@@ -1,10 +1,63 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import RadioGroup from '../radio-group';
 
-describe('Radio group', () => {
-  it('should render correctly', () => {
-    render(<RadioGroup />);
-    // ...
+describe('Radio Group', () => {
+  it('should have radio input disabled correctly', () => {
+    render(
+      <RadioGroup
+        id="sexo"
+        label="Sexo"
+        options={[
+          { id: 'macho', label: 'Macho', isDisabled: true },
+          { id: 'femea', label: 'Fêmea' },
+        ]}
+      />,
+    );
+    const radio = screen.getByLabelText('Macho');
+    expect(radio).toBeDisabled();
+  });
+
+  it('should render list options correctly', async () => {
+    const { getByText } = render(
+      <RadioGroup
+        id="sexo"
+        label="Sexo"
+        options={[
+          { id: 'macho', label: 'Macho' },
+          { id: 'femea', label: 'Fêmea' },
+        ]}
+      />,
+    );
+
+    const options = screen.getAllByRole('radio');
+
+    expect(getByText('Macho')).toBeInTheDocument();
+    expect(getByText('Fêmea')).toBeInTheDocument();
+
+    expect(options).toHaveLength(2);
+  });
+
+  it('handles click correctly', async () => {
+    render(
+      <RadioGroup
+        id="sexo"
+        label="Sexo"
+        options={[
+          { id: 'macho', label: 'Macho' },
+          { id: 'femea', label: 'Fêmea' },
+        ]}
+      />,
+    );
+
+    await fireEvent.click(screen.getByText('Macho'));
+
+    expect(screen.getByLabelText('Macho')).toBeChecked();
+  });
+
+  it('should be required', async () => {
+    render(<RadioGroup id="sexo" label="Sexo" isRequired options={[]} />);
+
+    expect(screen.getByText('*')).toBeInTheDocument();
   });
 });
--- src/components/common/radio-group/radio-group.tsx
+++ src/components/common/radio-group/radio-group.tsx
@@ -1,7 +1,30 @@
+import * as RadioBox from '@radix-ui/react-radio-group';
 import { FC } from 'react';
 
-interface Props {}
+import RadioInput, { RadioProps } from './radio-input';
 
-const RadioGroup: FC<Props> = () => null;
+interface Props {
+  id: string;
+  label: string;
+  options: RadioProps[];
+  isRequired?: boolean;
+}
+
+const RadioGroup: FC<Props> = ({ id, label, options, isRequired }) => {
+  return (
+    <div role="radiogroup">
+      <label htmlFor={id} className="mb-1 flex flex-row items-center gap-1 text-md font-bold text-primary-dark">
+        {label}
+        <span>{isRequired ? '*' : ''}</span>
+      </label>
+
+      <RadioBox.Root id={id} name={label} className="flex flex-wrap gap-4" required={isRequired}>
+        {options.map(({ id, label, isDisabled }) => (
+          <RadioInput key={id} id={id} label={label} isDisabled={isDisabled} />
+        ))}
+      </RadioBox.Root>
+    </div>
+  );
+};
 
 export default RadioGroup;
