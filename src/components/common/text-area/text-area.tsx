5734a872982a7b4a20f5d605cbb5a3cac093cfa6
felipe1496 <felipe.oliveira1496@gmail.com>
2022-07-03 14:54:27 -0300

    feat: adding id connection to textArea input and label

--- src/components/common/text-area/text-area.tsx
+++ src/components/common/text-area/text-area.tsx
@@ -3,11 +3,11 @@ import { FC } from 'react';
 
 interface Props extends React.HTMLProps<HTMLTextAreaElement> {}
 
-const TextArea: FC<Props> = ({ label, ...rest }) => {
+const TextArea: FC<Props> = ({ label, id, ...rest }) => {
   return (
     <div>
       {label && (
-        <Label className="text-md font-bold text-primary-dark">
+        <Label htmlFor={id} className="text-md font-bold text-primary-dark">
           {label}
           <div className="h-1" />
         </Label>
@@ -15,6 +15,7 @@ const TextArea: FC<Props> = ({ label, ...rest }) => {
 
       <textarea
         {...rest}
+        id={id}
         className="h-[7rem] w-full resize-none overflow-y-scroll rounded-pill border-2 border-neutral-100 bg-surface-primary p-4 text-md text-primary-dark scrollbar-thin scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar-track-transparent scrollbar-thumb-secondary-medium focus:border-secondary-medium focus:outline-none sm:h-[9.5rem]"
       />
     </div>
