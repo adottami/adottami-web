ebfeb04cb31e184255ff7a98d497f35cf54d7f84
Murilo Gruppi <murilo.castro@ccc.ufcg.edu.br>
2022-07-04 11:30:44 -0300

    tests: implements button tests

--- src/components/common/button/__tests__/button.test.tsx
+++ src/components/common/button/__tests__/button.test.tsx
@@ -1,10 +1,42 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import Button from '../button';
+import { buttonTestIds } from '../constants';
 
 describe('Button', () => {
-  it('should render correctly', () => {
-    render(<Button />);
-    // ...
+  describe('Primary button', () => {
+    it('should render primary button correctly', () => {
+      render(<Button variant="primary">Label</Button>);
+
+      expect(screen.getByTestId(buttonTestIds.primary())).toBeInTheDocument();
+    });
+
+    it('should render loading primary button correctly', () => {
+      render(<Button loading>Label</Button>);
+
+      const loadingIcon = screen.getByTestId(buttonTestIds.loadingIcon());
+      expect(loadingIcon).toBeInTheDocument();
+      expect(loadingIcon).not.toHaveAttribute('aria-hidden', 'true');
+    });
+  });
+
+  describe('Secondary button', () => {
+    it('should render secondary button correctly', () => {
+      render(<Button variant="secondary">Label</Button>);
+
+      expect(screen.getByTestId(buttonTestIds.secondary())).toBeInTheDocument();
+    });
+
+    it('should render loading secondary button correctly', () => {
+      render(
+        <Button variant="secondary" loading>
+          Label
+        </Button>,
+      );
+
+      const loadingIcon = screen.getByTestId(buttonTestIds.loadingIcon());
+      expect(loadingIcon).toBeInTheDocument();
+      expect(loadingIcon).not.toHaveAttribute('aria-hidden', 'true');
+    });
   });
 });
