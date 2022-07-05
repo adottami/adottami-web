98152b7744353fd3cac48c8a8fc03d8f584edcea
AmauriLima <amauri.plimaj@gmail.com>
2022-07-05 02:00:01 -0300

    feat: adds separator component

--- src/components/common/separator/separator.tsx
+++ src/components/common/separator/separator.tsx
@@ -1,7 +1,17 @@
-import { FC } from 'react';
+import { FunctionComponentWithChildren } from '@/types/react';
 
 interface Props {}
 
-const Separator: FC<Props> = () => null;
+const Separator: FunctionComponentWithChildren<Props> = ({ children }) => {
+  const gap = children ? 'gap-x-4' : '';
+
+  return (
+    <div
+      className={`my-4 flex items-center font-medium text-neutral-500 ${gap} before:h-px before:w-full before:bg-neutral-100 after:h-px after:w-full after:bg-neutral-100`}
+    >
+      {children}
+    </div>
+  );
+};
 
 export default Separator;
