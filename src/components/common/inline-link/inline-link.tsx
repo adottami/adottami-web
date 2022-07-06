b3c047f32e27f64f8f2150f383e92df230ac7af1
AmauriLima <amauri.plimaj@gmail.com>
2022-07-06 12:38:51 -0300

    feat: adds size options

--- src/components/common/inline-link/inline-link.tsx
+++ src/components/common/inline-link/inline-link.tsx
@@ -1,18 +1,26 @@
 import Link from 'next/link';
+import { CaretRight } from 'phosphor-react';
 import React from 'react';
 
 import { FunctionComponentWithChildren } from '@/types/react';
 
 interface Props {
   href: string;
+  rightIcon?: boolean;
+  size?: 'sm' | 'md' | 'lg';
 }
 
 const InlineLink: FunctionComponentWithChildren<Props> = (props) => {
-  const { href, children } = props;
+  const { href, children, rightIcon, size } = props;
 
   return (
-    <div className="after:hover: flex w-fit border-b border-transparent text-md leading-4 text-secondary-medium duration-200 hover:border-secondary-medium hover:brightness-75">
+    <div
+      className={`flex w-fit items-center border-b border-transparent
+        text-${size || 'md'} leading-4 text-secondary-medium duration-200
+        hover:cursor-pointer hover:border-secondary-medium hover:brightness-75`}
+    >
       <Link href={href}>{children}</Link>
+      {rightIcon && <CaretRight />}
     </div>
   );
 };
--- src/components/pages/home-page/home-page.tsx
+++ src/components/pages/home-page/home-page.tsx
@@ -1,16 +1,11 @@
 import { FC } from 'react';
 
-import InlineLink from '@/components/common/inline-link/inline-link';
-
 import { WELCOME_MESSAGE } from './constants';
 
 const HomePage: FC = () => (
-  <>
-    <div className="flex h-screen w-screen items-center justify-center bg-surface-primary">
-      <h1 className="text-4xl font-bold text-primary-dark">{WELCOME_MESSAGE}</h1>
-    </div>
-    <InlineLink href="/">Cadastre-se</InlineLink>
-  </>
+  <div className="flex h-screen w-screen items-center justify-center bg-surface-primary">
+    <h1 className="text-4xl font-bold text-primary-dark">{WELCOME_MESSAGE}</h1>
+  </div>
 );
 
 export default HomePage;
