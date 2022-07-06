d1c4f63d792a7e9ef15fbbaad3fa30c7dfc9968b
AmauriLima <amauri.plimaj@gmail.com>
2022-07-06 12:29:36 -0300

    css: adds underline on hover

--- src/components/common/inline-link/inline-link.tsx
+++ src/components/common/inline-link/inline-link.tsx
@@ -11,7 +11,7 @@ const InlineLink: FunctionComponentWithChildren<Props> = (props) => {
   const { href, children } = props;
 
   return (
-    <div className="flex text-md text-secondary-medium">
+    <div className="after:hover: flex w-fit border-b border-transparent text-md leading-4 text-secondary-medium duration-200 hover:border-secondary-medium hover:brightness-75">
       <Link href={href}>{children}</Link>
     </div>
   );
--- src/components/pages/home-page/home-page.tsx
+++ src/components/pages/home-page/home-page.tsx
@@ -1,11 +1,16 @@
 import { FC } from 'react';
 
+import InlineLink from '@/components/common/inline-link/inline-link';
+
 import { WELCOME_MESSAGE } from './constants';
 
 const HomePage: FC = () => (
-  <div className="flex h-screen w-screen items-center justify-center bg-surface-primary">
-    <h1 className="text-4xl font-bold text-primary-dark">{WELCOME_MESSAGE}</h1>
-  </div>
+  <>
+    <div className="flex h-screen w-screen items-center justify-center bg-surface-primary">
+      <h1 className="text-4xl font-bold text-primary-dark">{WELCOME_MESSAGE}</h1>
+    </div>
+    <InlineLink href="/">Cadastre-se</InlineLink>
+  </>
 );
 
 export default HomePage;
