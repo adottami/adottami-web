1e9b6df0929514b9b48888c5748a70755e3cbc85
Lucas Araújo de Lima <lucas.araujo.lima@ccc.ufcg.edu.br>
2022-07-06 20:57:28 -0300

    refactor: updated snapshot & added Separator (#14)

--- src/components/pages/sign-in/sign-in-page/sign-in-page.tsx
+++ src/components/pages/sign-in/sign-in-page/sign-in-page.tsx
@@ -4,15 +4,16 @@ import { FC } from 'react';
 
 import Button from '@/components/common/button/button';
 import Input from '@/components/common/input/input';
+import Separator from '@/components/common/separator/separator';
 import AdottamiLogo from '@/components/icons/adottami-logo';
 
 const SignInPage: FC = () => {
   return (
     <div className="flex min-h-screen w-full overflow-y-auto">
-      <div className="mx-[1.5rem] my-[1rem] flex h-full w-full flex-col items-start gap-8 md:m-[3.5rem] md:w-1/2 xl:m-[6rem]">
-        <AdottamiLogo className="h-6 w-auto md:h-8" />
+      <div className="mx-[1.5rem] my-[1rem] flex h-full w-full flex-col items-start gap-8 md:m-[3.5rem] md:w-1/2 xl:m-[6rem] 2xl:gap-14">
+        <AdottamiLogo className="h-6 w-auto md:h-8 2xl:h-10" />
 
-        <div className="w-full md:w-3/4">
+        <div className="w-full md:w-3/4 xl:w-4/6">
           <div className="flex flex-col gap-4 md:gap-6">
             <h3 className="w-full text-xl font-bold text-primary-dark md:text-2xl">Acesse a sua conta</h3>
             <div className="flex flex-col gap-4 md:gap-8">
@@ -22,17 +23,19 @@ const SignInPage: FC = () => {
               </div>
               <Button size="md">Entrar</Button>
             </div>
-            <p className="text-center text-sm text-neutral-500">
-              Ao continuar, você concorda com os <a>Termos de Uso</a> e a <a>Política de Privacidade</a> da Adottami, e
-              também, em receber comunicações via e-mail.
-            </p>
-            <hr className="w-full border-neutral-100" />
-            <p className="w-full text-center text-sm text-neutral-800 md:text-md">
-              Não tem uma conta ?{' '}
-              <a href="" className="text-sm text-secondary-medium md:text-md">
-                Cadastre-se
-              </a>
-            </p>
+            <div className="flex flex-col gap-4 md:gap-6">
+              <p className="text-center text-sm text-neutral-500 md:text-md">
+                Ao continuar, você concorda com os <a>Termos de Uso</a> e a <a>Política de Privacidade</a> da Adottami,
+                e também, em receber comunicações via e-mail.
+              </p>
+              <Separator />
+              <p className="w-full text-center text-sm text-neutral-800 md:text-md">
+                Não tem uma conta ?{' '}
+                <a href="" className="text-sm text-secondary-medium md:text-md">
+                  Cadastre-se
+                </a>
+              </p>
+            </div>
           </div>
         </div>
       </div>
