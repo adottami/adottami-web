66ffa3ea9794485df00a020d4c9dfcec31dd5296
AmauriLima <amauri.plimaj@gmail.com>
2022-07-08 10:29:48 -0300

    fix: remove size props

--- src/components/common/inline-link/inline-link.tsx
+++ src/components/common/inline-link/inline-link.tsx
@@ -4,21 +4,18 @@ import React from 'react';
 
 import { FunctionComponentWithChildren } from '@/types/react';
 
-type Size = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl' | '4xl';
-
 interface Props {
   href: string;
   rightIcon?: boolean;
-  size?: Size;
 }
 
 const InlineLink: FunctionComponentWithChildren<Props> = (props) => {
-  const { href, children, rightIcon, size } = props;
+  const { href, children, rightIcon } = props;
 
   return (
     <div
       className={`flex w-fit items-center border-b border-transparent
-        text-${size || 'md'} leading-4 text-secondary-medium duration-200
+        text-md leading-4 text-secondary-medium duration-200
         hover:cursor-pointer hover:border-secondary-medium hover:brightness-75`}
     >
       <Link href={href}>{children}</Link>
