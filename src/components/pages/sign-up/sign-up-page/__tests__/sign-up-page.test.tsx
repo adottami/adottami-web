65d2a1c11c650c30522625101ecdf3d176fff4e1
Lucas Araújo de Lima <lucas.araujo.lima@ccc.ufcg.edu.br>
2022-07-09 09:17:32 -0300

    test: created snapshot test

--- src/components/pages/sign-up/sign-up-page/__tests__/sign-up-page.test.tsx
+++ src/components/pages/sign-up/sign-up-page/__tests__/sign-up-page.test.tsx
@@ -1,10 +1,10 @@
-import { render } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 
 import SignUpPage from '../sign-up-page';
 
 describe('Sign up page', () => {
   it('should render correctly', () => {
-    render(<SignUpPage />);
-    // ...
+    const tree = renderer.create(<SignUpPage />).toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
