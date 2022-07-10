63e7390d9bcdc5f4afda66d5a76f80340004f96e
felipe1496 <felipe.oliveira1496@gmail.com>
2022-07-10 18:29:09 -0300

    test: adding file-input component tests

---
+++ src/components/common/file-input/__tests__/file-input.test.tsx
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react';
+
+import FileInput from '../file-input';
+
+describe('File Input', () => {
+  it('should render correctly', () => {
+    render(<FileInput />);
+    // ...
+  });
+});
