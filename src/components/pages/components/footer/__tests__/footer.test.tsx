b0edbeb4313b898378495cc67edfe88fe0011e52
Murilo Gruppi <murilo.castro@ccc.ufcg.edu.br>
2022-07-11 12:52:22 -0300

    feat: create footer component

--- src/components/pages/components/footer/__tests__/footer.test.tsx
+++ src/components/pages/components/footer/__tests__/footer.test.tsx
@@ -1,10 +1,16 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
+import { footerTestIds } from '../constants';
 import Footer from '../footer';
 
 describe('Footer', () => {
-  it('should render correctly', () => {
+  it('should render footer correctly', () => {
     render(<Footer />);
-    // ...
+
+    expect(screen.getByTestId(footerTestIds.logo())).toBeInTheDocument();
+    expect(screen.getByTestId(footerTestIds.description())).toBeInTheDocument();
+    expect(screen.getByTestId(footerTestIds.inlineLinks())).toBeInTheDocument();
+    expect(screen.getByTestId(footerTestIds.copyright())).toBeInTheDocument();
+    expect(screen.getByTestId(footerTestIds.policyAndTerms())).toBeInTheDocument();
   });
 });
