8f2d0944f18b89d7971c0ffba314379d60bb9109
felipe1496 <felipe.oliveira1496@gmail.com>
2022-07-10 22:16:30 -0300

    test: adding tests of file-input component

--- src/components/common/file-input/__tests__/file-input.test.tsx
+++ src/components/common/file-input/__tests__/file-input.test.tsx
@@ -1,10 +1,26 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import FileInput from '../file-input';
 
 describe('File Input', () => {
-  it('should render correctly', () => {
-    render(<FileInput />);
-    // ...
+  describe('Variant Image File Input', () => {
+    it('should not show preview if no image has been selected', () => {
+      render(<FileInput />);
+      expect(screen.queryByTestId('fileImage')).not.toBeInTheDocument();
+    });
+    it('should render label correctly', () => {
+      render(<FileInput variant="image" label="Some Label" />);
+
+      const fileInput = screen.getByText('Some Label');
+
+      expect(fileInput).toBeInTheDocument();
+    });
+    it('should render description correctly', () => {
+      render(<FileInput variant="image" description="Some Description" label="Some Label" />);
+
+      const description = screen.getByText('Some Description');
+
+      expect(description).toBeInTheDocument();
+    });
   });
 });
