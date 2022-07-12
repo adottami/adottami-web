f9b0c82cac12318441a9fff77e76847e1c43632c
AmauriLima <amauri.plimaj@gmail.com>
2022-07-12 00:50:00 -0300

    fix: removes publication card from home page

--- src/components/pages/home-page/home-page.tsx
+++ src/components/pages/home-page/home-page.tsx
@@ -1,16 +1,11 @@
 import { FC } from 'react';
 
-import { publicationMock } from '../components/publication-card/mock';
-import PublicationCard from '../components/publication-card/publication-card';
 import { WELCOME_MESSAGE } from './constants';
 
 const HomePage: FC = () => (
-  <>
-    <div className="flex h-screen w-screen items-center justify-center bg-surface-primary">
-      <h1 className="text-4xl font-bold text-primary-dark">{WELCOME_MESSAGE}</h1>
-    </div>
-    <PublicationCard publication={publicationMock} />
-  </>
+  <div className="flex h-screen w-screen items-center justify-center bg-surface-primary">
+    <h1 className="text-4xl font-bold text-primary-dark">{WELCOME_MESSAGE}</h1>
+  </div>
 );
 
 export default HomePage;
