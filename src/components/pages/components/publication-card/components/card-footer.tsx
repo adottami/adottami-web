9bec4eeb9f476349efaa3ae61c43981eb3daf544
AmauriLima <amauri.plimaj@gmail.com>
2022-07-12 00:42:02 -0300

    feat: adjusts card footer styles

--- src/components/pages/components/publication-card/components/card-footer.tsx
+++ src/components/pages/components/publication-card/components/card-footer.tsx
@@ -1,17 +1,29 @@
-import { GenderFemale } from 'phosphor-react';
-import { FC } from 'react';
+import { GenderFemale, GenderMale } from 'phosphor-react';
+import { FC, useMemo } from 'react';
 
 interface Props {
   gender: string;
 }
 
+const Icon: FC<Props> = (props) => {
+  const { gender } = props;
+
+  if (gender.toLowerCase() === 'fêmea') {
+    return <GenderFemale size={24} />;
+  }
+
+  return <GenderMale size={24} />;
+};
+
 const CardFooter: FC<Props> = (props) => {
   const { gender } = props;
 
+  const color = useMemo(() => (gender.toLowerCase() === 'fêmea' ? 'tertiary-medium' : 'secondary-medium'), [gender]);
+
   return (
-    <footer>
-      <span>
-        <GenderFemale />
+    <footer className="mt-2">
+      <span className={`text-${color} flex w-fit items-center gap-2 rounded-full border-2 border-${color} px-3 py-2`}>
+        <Icon gender={gender} />
         {gender}
       </span>
     </footer>
