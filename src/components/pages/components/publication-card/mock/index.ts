fff7c4ae993c0719f7b6b81aa57d007fdbfd79b8
AmauriLima <amauri.plimaj@gmail.com>
2022-07-11 23:23:59 -0300

    feat: adds publication card first structure

---
+++ src/components/pages/components/publication-card/mock/index.ts
@@ -0,0 +1,36 @@
+import Publication from '@/models/publication/publication';
+
+export const publicationMock = new Publication({
+  name: 'Moly',
+  gender: 'Fêmea',
+  city: 'Campina Grande',
+  state: 'Catolé',
+  id: '1-1-1-1',
+  images: [
+    {
+      id: '1',
+      url: 'https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    },
+    {
+      id: '2',
+      url: 'https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    },
+    {
+      id: '3',
+      url: 'https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    },
+    {
+      id: '4',
+      url: 'https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    },
+  ],
+  author: { email: 'eu@eu.com', id: '1', name: 'eu', phoneNumber: '99999999999' },
+  breed: '01/06/2022',
+  ageInYears: 1,
+  category: 'qualquer',
+  characteristics: [{ id: '1', name: 'teste' }],
+  description: 'Um animal qualquer',
+  isArchived: false,
+  weightInGrams: 1200,
+  zipCode: '1111',
+});
--- src/components/pages/components/publication-card/publication-card.tsx
+++ src/components/pages/components/publication-card/publication-card.tsx
@@ -1,7 +1,59 @@
+import { CalendarBlank, Camera, GenderFemale, Heart, MapPinLine } from 'phosphor-react';
 import { FC } from 'react';
 
-interface Props {}
+import Publication from '@/models/publication/publication';
+import PublicationFactory from '@/models/publication/publication-factory';
 
-const PublicationCard: FC<Props> = () => null;
+interface Props {
+  publication: Publication;
+}
+
+const PublicationCard: FC<Props> = (props) => {
+  const { publication } = props;
+  const { name, images, city, state, breed, gender } = PublicationFactory.toResponse(publication);
+
+  function getLocation() {
+    return [city, state].join(', ');
+  }
+
+  return (
+    <div>
+      <header>
+        <img src={images[0].url} alt="" />
+        <div>
+          <Camera />
+          <span>{images.length} Foto(s)</span>
+        </div>
+      </header>
+
+      <div>
+        <div>
+          <h3>{name}</h3>
+          <span>
+            <Heart />
+          </span>
+        </div>
+
+        <div>
+          <div>
+            <MapPinLine />
+            {getLocation()}
+          </div>
+          <div>
+            <CalendarBlank />
+            {breed}
+          </div>
+        </div>
+
+        <footer>
+          <span>
+            <GenderFemale />
+            {gender}
+          </span>
+        </footer>
+      </div>
+    </div>
+  );
+};
 
 export default PublicationCard;
