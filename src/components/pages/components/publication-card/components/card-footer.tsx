9977317fa6d0b0576a02cdf92881184f8e8edd35
AmauriLima <amauri.plimaj@gmail.com>
2022-07-12 16:33:55 -0300

    refactor: removes unnecessary useMemo and class concatenation

--- src/components/pages/components/publication-card/components/card-footer.tsx
+++ src/components/pages/components/publication-card/components/card-footer.tsx
@@ -1,5 +1,5 @@
 import { GenderFemale, GenderMale } from 'phosphor-react';
-import { FC, useMemo } from 'react';
+import { FC } from 'react';
 
 interface Props {
   gender: string;
@@ -13,11 +13,14 @@ const Icon: FC<Props> = (props) => {
 const CardFooter: FC<Props> = (props) => {
   const { gender } = props;
 
-  const color = useMemo(() => (gender.toLowerCase() === 'fêmea' ? 'tertiary-medium' : 'secondary-medium'), [gender]);
+  const { color, borderColor } =
+    gender.toLowerCase() === 'fêmea'
+      ? { color: 'text-tertiary-medium', borderColor: 'border-tertiary-medium' }
+      : { color: 'text-secondary-medium', borderColor: 'border-secondary-medium' };
 
   return (
     <footer className="mt-2">
-      <span className={`text-${color} flex w-fit items-center gap-2 rounded-full border-2 border-${color} px-3 py-2`}>
+      <span className={`${color} ${borderColor} flex w-fit items-center gap-2 rounded-full border-2 px-3 py-2`}>
         <Icon gender={gender} />
         {gender}
       </span>
