6778c994a4c35e52d5a998fb13ee3d6224c1fd06
AmauriLima <amauri.plimaj@gmail.com>
2022-07-12 00:51:00 -0300

    fix: first test

--- src/components/pages/components/publication-card/__tests__/publication-card.test.tsx
+++ src/components/pages/components/publication-card/__tests__/publication-card.test.tsx
@@ -1,10 +1,10 @@
 import { render } from '@testing-library/react';
 
+import { publicationMock } from '../mock';
 import PublicationCard from '../publication-card';
 
 describe('Publication card', () => {
   it('should render correctly', () => {
-    render(<PublicationCard />);
-    // ...
+    render(<PublicationCard publication={publicationMock} />);
   });
 });
