acc1b4a6b5b05e0b16d2a9e8457734fa66591e4f
emanamoura <emanuel.moura@ccc.ufcg.edu.br>
2022-07-13 12:55:14 -0300

    feat(color when hover): adding color when hover an link

--- src/components/pages/components/header/header.tsx
+++ src/components/pages/components/header/header.tsx
@@ -1,9 +1,20 @@
 import Image, { StaticImageData } from 'next/image';
 import Link from 'next/link';
 import { NextRouter, useRouter } from 'next/router';
-import { CaretRight, Door, Heart, List, MagnifyingGlass, Shield, SquaresFour, User, X } from 'phosphor-react';
+import {
+  CaretDown,
+  CaretRight,
+  Door,
+  Heart,
+  List,
+  MagnifyingGlass,
+  Shield,
+  SquaresFour,
+  User,
+  X,
+} from 'phosphor-react';
 import profile from 'public/images/image-profile-not-found.png';
-import { FC, useState, cloneElement } from 'react';
+import { FC, useState } from 'react';
 
 import Button from '@/components/common/button/button';
 import Separator from '@/components/common/separator/separator';
@@ -29,13 +40,15 @@ interface CardProps {
 
 const MenuCard: FC<CardProps> = ({ router, icon, href, text }) => {
   return (
-    <li className="flex items-center">
-      {cloneElement(icon, {
-        color: `${router.pathname === href ? '#6E3ED8' : '#4A4A4A'}`,
-      })}
+    <li
+      className={`flex items-center ${
+        router.pathname === href ? 'text-secondary-medium' : 'text-neutral-800'
+      } hover:text-secondary-medium`}
+    >
+      {icon}
       <Link href={href} passHref>
         <a
-          className={`ml-2.5 text-lg font-medium ${
+          className={`ml-2.5 text-lg font-medium hover:text-secondary-medium ${
             router.pathname === href ? 'text-secondary-medium' : 'text-neutral-800'
           } `}
         >
@@ -51,17 +64,19 @@ const ModalMenu: FC<ModalMenu> = ({ setCloseModal, isAuth, avatarPhoto = profile
     <nav className="fixed left-0 top-0 z-1 h-full w-full bg-surface-primary pt-6 pl-7 shadow-sm">
       <ul className="flex flex-col gap-6">
         <li className="self-end pr-2">
-          <X size={24} weight="bold" onClick={setCloseModal} />
+          <X size={24} weight="bold" onClick={setCloseModal} className="hover:text-secondary-medium" />
         </li>
-        <li className="flex items-center">
-          <Image src={avatarPhoto} alt="Foto de perfil" width={42} height={42} className="rounded-full" />
-          <div className="pl-4">
-            <p>{username}</p>
-            <Link href="#">
-              <a className="text-sm text-secondary-medium">Minha conta</a>
-            </Link>
+        <li className="flex items-center justify-between">
+          <div className="flex items-center">
+            <Image src={avatarPhoto} alt="Foto de perfil" width={42} height={42} className="rounded-full" />
+            <div className="pl-4">
+              <p>{username}</p>
+              <Link href="#">
+                <a className="text-sm text-secondary-medium">Minha conta</a>
+              </Link>
+            </div>
           </div>
-          <CaretRight size={24} weight="thin" style={{ flexGrow: 1 }} />
+          <CaretRight size={24} weight="thin" className="hover:text-secondary-medium" />
         </li>
         <Separator />
         <MenuCard
@@ -88,7 +103,7 @@ const ModalMenu: FC<ModalMenu> = ({ setCloseModal, isAuth, avatarPhoto = profile
     <nav className="fixed left-0 top-0 z-1 h-full w-full bg-surface-primary pt-6 pl-7 shadow-sm">
       <ul className="flex flex-col gap-6">
         <li className="self-end pr-2">
-          <X size={32} weight="bold" onClick={setCloseModal} />
+          <X size={32} weight="bold" onClick={setCloseModal} className="hover:text-secondary-medium" />
         </li>
         <MenuCard router={router} icon={<User size={20} weight="thin" />} href="/sign-in" text="Entrar" />
         <MenuCard
@@ -131,7 +146,7 @@ const Header: FC<Props> = ({ isAuth, avatarPhoto = profile, username }) => {
           </span>
           <Link href="/">
             <div>
-              <AdottamiLogo className="h-6 w-auto cursor-pointer lg:h-8 2xl:h-10" />
+              <AdottamiLogo className="hidden w-auto cursor-pointer lg:block lg:h-8 2xl:h-10" />
             </div>
           </Link>
         </div>
@@ -155,6 +170,7 @@ const Header: FC<Props> = ({ isAuth, avatarPhoto = profile, username }) => {
                     <Link href="#" className="text-lg font-medium">
                       <a className="ml-2.5 text-lg font-medium text-neutral-800">{username}</a>
                     </Link>
+                    <CaretDown size={20} className="hover:text-secondary-medium" />
                   </>
                 ) : (
                   <>
