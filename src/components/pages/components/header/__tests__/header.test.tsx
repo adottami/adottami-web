fc736a90623f42d995e34543e16007217a0512d9
emanamoura <emanuel.moura@ccc.ufcg.edu.br>
2022-07-13 20:44:55 -0300

    fix(test): removing properties that were not necessary

--- src/components/pages/components/header/__tests__/header.test.tsx
+++ src/components/pages/components/header/__tests__/header.test.tsx
@@ -6,17 +6,7 @@ import Header from '../header';
 jest.mock('next/router', () => ({
   useRouter() {
     return {
-      route: '/',
       pathname: '/',
-      query: '',
-      asPath: '',
-      push: jest.fn(),
-      events: {
-        on: jest.fn(),
-        off: jest.fn(),
-      },
-      beforePopState: jest.fn(() => null),
-      prefetch: jest.fn(() => null),
     };
   },
 }));
