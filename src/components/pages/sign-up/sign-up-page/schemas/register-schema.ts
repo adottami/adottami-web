4ca5278ea6285ea5999e10779c95f649b16cd441
Lucas Araújo de Lima <lucas.araujo.lima@ccc.ufcg.edu.br>
2022-07-15 00:13:48 -0300

    feat: created RegisterSchema to validate correct entries

---
+++ src/components/pages/sign-up/sign-up-page/schemas/register-schema.ts
@@ -0,0 +1,14 @@
+import * as Yup from 'yup';
+
+export const RegisterSchema = Yup.object().shape({
+  name: Yup.string().required('Nome é obrigatório'),
+  email: Yup.string().email('E-mail inválido').required('Email é obrigatório'),
+  telephone: Yup.string()
+    .max(14, 'Telefone inválido')
+
+    .required('Telefone é obrigatório'),
+  password: Yup.string().min(6, 'A senha deve conter no minimo 6 caracteres').required('Senha é obrigatória'),
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref('password'), null], 'As senhas não conferem')
+    .required('Confirmação de senha é obrigatória'),
+});
