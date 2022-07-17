f66262f4b59485619d22128e1ae1fc9fb271e754
AmauriLima <amauri.plimaj@gmail.com>
2022-07-17 14:58:10 -0300

    feat: adds handleFieldChange function

--- src/components/pages/components/publication-form/publication-form.tsx
+++ src/components/pages/components/publication-form/publication-form.tsx
@@ -2,9 +2,15 @@ import { FC } from 'react';
 
 import { PublicationFields } from '@/models/publication/types';
 
+export interface OnFieldChangeProps {
+  field: keyof PublicationFields;
+  value: PublicationFields[keyof PublicationFields];
+}
+
 interface Props {
   header: string;
   formData: PublicationFields;
+  onFieldChange(props: OnFieldChangeProps): void;
 }
 
 const PublicationForm: FC<Props> = () => {
--- src/components/pages/publications/create/create-publication-page/create-publication-page.tsx
+++ src/components/pages/publications/create/create-publication-page/create-publication-page.tsx
@@ -2,7 +2,7 @@ import { FC, useState } from 'react';
 
 import Page from '@/components/common/page/page';
 import FormPageHeader from '@/components/pages/components/form-page-header/form-pageheader';
-import PublicationForm from '@/components/pages/components/publication-form/publication-form';
+import PublicationForm, { OnFieldChangeProps } from '@/components/pages/components/publication-form/publication-form';
 import { PublicationFields } from '@/models/publication/types';
 
 import FormFooter from './components/form-footer';
@@ -11,11 +11,24 @@ import { PAGE_TITLE } from './constants';
 const CreatePublicationPage: FC = () => {
   const [publication, setPublication] = useState<PublicationFields>({} as PublicationFields);
 
+  function handleFieldChange(props: OnFieldChangeProps) {
+    const { field, value } = props;
+
+    setPublication((prevState) => ({
+      ...prevState,
+      [field]: value,
+    }));
+  }
+
   return (
     <Page title={PAGE_TITLE}>
       <FormPageHeader />
 
-      <PublicationForm formData={publication} header="Qual pet você está anunciando?" />
+      <PublicationForm
+        formData={publication}
+        onFieldChange={handleFieldChange}
+        header="Qual pet você está anunciando?"
+      />
 
       <FormFooter onSubmit={() => {}} />
     </Page>
