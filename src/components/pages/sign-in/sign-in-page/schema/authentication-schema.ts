617c49e9d82ee1261313bcffbda57c50dfa809de
Lucas Araújo de Lima <lucas.araujo.lima@ccc.ufcg.edu.br>
2022-07-17 15:57:32 -0300

    rafactor: adjust name to authenticationSchema

--- src/components/pages/sign-in/sign-in-page/schema/authentication-schema.ts
+++ src/components/pages/sign-in/sign-in-page/schema/authentication-schema.ts
@@ -1,6 +1,6 @@
 import * as yup from 'yup';
 
-export const AuthenticationSchema = yup.object().shape({
+export const authenticationSchema = yup.object().shape({
   email: yup.string().email('E-mail inválido').required('E-mail é obrigatório'),
   password: yup.string().min(6, 'Senha deve ter no minimo 6 caracteres').required('Senha é obrigatória'),
 });
