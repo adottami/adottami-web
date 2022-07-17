be078e97a0886616b828eb3d0b397cee77d7ad18
Lucas Araújo de Lima <lucas.araujo.lima@ccc.ufcg.edu.br>
2022-07-17 15:56:34 -0300

    eat: created TOAST_CONFIGS constant

--- src/components/pages/sign-in/sign-in-page/constants.ts
+++ src/components/pages/sign-in/sign-in-page/constants.ts
@@ -1,1 +1,9 @@
 export const PAGE_TITLE = 'Acessar conta';
+
+export const TOAST_CONFIGS = {
+  autoClose: 3000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+};
