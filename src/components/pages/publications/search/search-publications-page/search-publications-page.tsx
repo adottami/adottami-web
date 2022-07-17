362168227bae0edef190477b5838845e33990faa
Felipe Oliveira <felipe.oliveira.silva@ccc.ufcg.edu.br>
2022-07-17 13:39:59 -0300

    feat: adding location input to search publication page

--- src/components/pages/publications/search/search-publications-page/search-publications-page.tsx
+++ src/components/pages/publications/search/search-publications-page/search-publications-page.tsx
@@ -1,5 +1,17 @@
 import { FC } from 'react';
 
-const SearchPublicationsPage: FC = () => null;
+import LocationInput from '@/components/pages/publications/search/components/location-input/location-input';
+
+const SearchPublicationsPage: FC = () => {
+  return (
+    <div className="flex h-screen w-full flex-col items-center">
+      <div className="flex h-60 w-full items-center justify-center bg-secondary-medium md:h-32">
+        <div className="w-4/5">
+          <LocationInput />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default SearchPublicationsPage;
