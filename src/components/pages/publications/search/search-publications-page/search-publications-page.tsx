f3b9245b7ba2190ac7d676f5c89bbc10af1e7f3c
Felipe Oliveira <felipe.oliveira.silva@ccc.ufcg.edu.br>
2022-07-17 16:57:43 -0300

    fix: readding Page component

--- src/components/pages/publications/search/search-publications-page/search-publications-page.tsx
+++ src/components/pages/publications/search/search-publications-page/search-publications-page.tsx
@@ -1,16 +1,22 @@
 import { FC } from 'react';
 
+import Page from '@/components/common/page/page';
 import LocationInput from '@/components/pages/publications/search/components/location-input/location-input';
+
+import Categories from '../components/Categories/categories';
+import { PAGE_TITLE } from './constants';
 
 const SearchPublicationsPage: FC = () => {
   return (
-    <div className="flex h-screen w-full flex-col items-center">
-      <div className="flex h-60 w-full items-center justify-center bg-secondary-medium md:h-32">
-        <div className="w-4/5">
-          <LocationInput />
+    <Page title={PAGE_TITLE}>
+      <div className="min-w-screen flex h-screen flex-col">
+        <div className="flex h-60 w-full items-center justify-center bg-secondary-medium md:h-32">
+          <div className="w-4/5">
+            <LocationInput />
+          </div>
         </div>
       </div>
-    </div>
+    </Page>
   );
 };
 
