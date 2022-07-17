231fab2c54b4a5dbe4c071bd501dc426d9defa13
Felipe Oliveira <felipe.oliveira.silva@ccc.ufcg.edu.br>
2022-07-17 19:11:23 -0300

    feat: adding categories component

---
+++ src/components/pages/publications/search/components/Categories/categories.tsx
@@ -0,0 +1,51 @@
+import { FC } from 'react';
+
+import CategoryIcon from '@/components/icons/category-icon';
+
+const categoriesInfo = [
+  {
+    variant: 'dog',
+    name: 'Cachorro',
+  },
+  {
+    variant: 'cat',
+    name: 'Gato',
+  },
+  {
+    variant: 'rabbit',
+    name: 'Coelho',
+  },
+  {
+    variant: 'hamster',
+    name: 'Hamster',
+  },
+  {
+    variant: 'bird',
+    name: 'Passaro',
+  },
+  {
+    variant: 'fish',
+    name: 'Peixe',
+  },
+  {
+    name: 'Outros',
+  },
+];
+
+const Categories: FC = () => {
+  return (
+    <div className="flex gap-4 md:grid md:grid-cols-4">
+      {categoriesInfo.map(({ variant, name }) => (
+        <button
+          type="button"
+          className="flex h-12 w-32 items-center justify-center gap-2 scroll-auto rounded-pill border-2 border-neutral-100 px-5 md:w-4/5"
+        >
+          <CategoryIcon variant={variant} width="1.6em" height="1.6em" />
+          <span className="text-neutral-800 md:text-lg">{name}</span>
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default Categories;
