ce1ae99928afa8125132ade8dc2154d8d452249b
Felipe Oliveira <felipe.oliveira.silva@ccc.ufcg.edu.br>
2022-07-17 17:20:35 -0300

    fix: reutilizing Button component

--- src/components/pages/publications/search/components/location-input/location-input.tsx
+++ src/components/pages/publications/search/components/location-input/location-input.tsx
@@ -1,6 +1,7 @@
 import { MagnifyingGlass } from 'phosphor-react';
 import { FC, FormEvent, useEffect, useState } from 'react';
 
+import Button from '@/components/common/button/button';
 import Input from '@/components/common/input/input';
 
 interface Location {
@@ -32,13 +33,10 @@ const LocationInput: FC = () => {
     <form onSubmit={handleSendForm} className="flex w-full flex-col gap-4 md:flex-row md:justify-between">
       <Input variant="searchLocation" placeholder="Estado" id="state" type="text" name="state" />
       <Input variant="searchLocation" placeholder="Cidade" id="city" type="text" name="city" />
-      <button
-        type="submit"
-        className="flex h-[46px] w-full items-center justify-center gap-3 rounded-pill bg-positive-medium text-surface-primary focus:border-2 focus:border-black focus:outline-none md:h-[54px] md:w-32"
-      >
+      <Button type="submit" variant="positive">
         <MagnifyingGlass size={20} />
         <span className="text-sm  md:hidden md:text-md 2xl:text-md">Buscar</span>
-      </button>
+      </Button>
     </form>
   );
 };
