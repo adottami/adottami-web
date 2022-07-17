e9573571a309baf7a7ffa41f70a590766105a0d2
Felipe Oliveira <felipe.oliveira.silva@ccc.ufcg.edu.br>
2022-07-17 16:58:31 -0300

    fix: readding page title test

--- src/components/pages/publications/search/search-publications-page/__tests__/search-publications-page.test.tsx
+++ src/components/pages/publications/search/search-publications-page/__tests__/search-publications-page.test.tsx
@@ -1,9 +1,13 @@
 import { render } from '@testing-library/react';
 
+import { expectPageTitleWithApplicationName } from '@/components/common/page/__tests__/utils';
+
+import { PAGE_TITLE } from '../constants';
 import SearchPublicationsPage from '../search-publications-page';
 
 describe('Search publication page', () => {
   it('should render correctly', () => {
     render(<SearchPublicationsPage />);
+    expectPageTitleWithApplicationName(PAGE_TITLE);
   });
 });
