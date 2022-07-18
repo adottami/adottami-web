24effe3c167b9906127e460c3594e42159043e14
AmauriLima <amauri.plimaj@gmail.com>
2022-07-18 03:35:54 -0300

    fix: remove unncessary comment

--- src/components/common/inline-link/inline-link.tsx
+++ src/components/common/inline-link/inline-link.tsx
@@ -15,7 +15,6 @@ const InlineLink: FunctionComponentWithChildren<Props> = (props) => {
 
   if (href) {
     return (
-      // eslint-disable-next-line react/jsx-no-useless-fragment
       <Link href={href} passHref>
         <a className="flex w-fit cursor-pointer items-center gap-1 text-sm leading-4 text-secondary-medium no-underline decoration-secondary-medium transition duration-300 ease-out hover:text-secondary-dark hover:underline md:text-md">
           {children}
