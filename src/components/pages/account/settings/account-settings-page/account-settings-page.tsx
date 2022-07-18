43671ee94ec681a3641afe4b94edd509590aa7b0
AmauriLima <amauri.plimaj@gmail.com>
2022-07-18 03:41:15 -0300

    refactor: turns navbar into component

--- src/components/pages/account/settings/account-settings-page/account-settings-page.tsx
+++ src/components/pages/account/settings/account-settings-page/account-settings-page.tsx
@@ -1,4 +1,3 @@
-import { Shield, User } from 'phosphor-react';
 import { FC, useState } from 'react';
 
 import Page from '@/components/common/page/page';
@@ -7,31 +6,11 @@ import Header from '@/components/pages/components/header/header';
 
 import LoginAndSecurity from './components/login-and-security/login-and-security';
 import MyCadastre from './components/my-cadastre/my-cadastre';
+import NavBar, { PageType } from './components/navbar';
 import { PAGE_TITLE } from './constants';
-
-type PageType = 'my-cadastre' | 'security-and-login';
 
 const AccountSettingsPage: FC = () => {
   const [selectedPage, setSelectedPage] = useState<PageType>('my-cadastre');
-
-  const navBarOptions = [
-    {
-      icon: <User size={24} />,
-      label: 'Meu cadastro',
-      isSelected: selectedPage === 'my-cadastre',
-      onClick: () => setSelectedPage('my-cadastre'),
-    },
-    {
-      icon: <Shield size={24} />,
-      label: 'Login e segurança',
-      isSelected: selectedPage === 'security-and-login',
-      onClick: () => setSelectedPage('security-and-login'),
-    },
-  ];
-
-  const optionColors = (isSelected: boolean) => {
-    return isSelected ? 'bg-secondary-medium/[0.15] text-secondary-medium' : 'text-neutral-500';
-  };
 
   return (
     <Page title={PAGE_TITLE}>
@@ -39,26 +18,7 @@ const AccountSettingsPage: FC = () => {
         <Header isAuth username="Matheus" />
 
         <div className="mx-auto mt-14 mb-24 flex w-full max-w-4xl justify-between gap-8 px-8">
-          <div className="mt-4 hidden lg:flex">
-            <nav>
-              <ul className="flex flex-col gap-4">
-                {navBarOptions.map((option) => (
-                  <li>
-                    <button
-                      type="button"
-                      onClick={option.onClick}
-                      className={`flex w-full gap-4 rounded-full ${optionColors(
-                        option.isSelected,
-                      )} cursor-pointer py-2 px-4`}
-                    >
-                      {option.icon}
-                      {option.label}
-                    </button>
-                  </li>
-                ))}
-              </ul>
-            </nav>
-          </div>
+          <NavBar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
 
           {selectedPage === 'my-cadastre' ? <MyCadastre /> : <LoginAndSecurity />}
         </div>
