ec9e9921e0307a535142575bf2abb8b08a949715
AmauriLima <amauri.plimaj@gmail.com>
2022-07-18 02:39:00 -0300

    feat: adds my cadastre form ayout

--- src/components/pages/account/settings/account-settings-page/components/box.tsx
+++ src/components/pages/account/settings/account-settings-page/components/box.tsx
@@ -1,5 +1,18 @@
 import { FCC } from '@/types/react';
 
-const Box: FCC = ({ children }) => <div className="rounded-3 bg-white p-6 drop-shadow">{children}</div>;
+interface Props {
+  title?: string;
+}
+
+const Box: FCC<Props> = (props) => {
+  const { children, title } = props;
+
+  return (
+    <div className="rounded-3 bg-white p-6 drop-shadow">
+      {title && <h3 className="mb-4 text-lg font-bold text-neutral-800">{title}</h3>}
+      {children}
+    </div>
+  );
+};
 
 export default Box;
--- src/components/pages/account/settings/account-settings-page/components/my-cadastre/my-cadastre.tsx
+++ src/components/pages/account/settings/account-settings-page/components/my-cadastre/my-cadastre.tsx
@@ -1,5 +1,8 @@
 import { FC } from 'react';
 
+import Button from '@/components/common/button/button';
+import Input from '@/components/common/input/input';
+
 import Box from '../box';
 import DefaultSection from '../default-section';
 
@@ -8,7 +11,16 @@ interface Props {}
 const MyCadastre: FC<Props> = () => {
   return (
     <DefaultSection title="Meu cadastro" description="Configure o seu cadastro.">
-      <Box>teste</Box>
+      <Box title="Dados da conta">
+        <form className="flex flex-col gap-4">
+          <Input label="Nome completo" isRequired />
+          <Input label="Telefone" isRequired />
+
+          <div className="mt-4">
+            <Button type="submit">Salvar alterações</Button>
+          </div>
+        </form>
+      </Box>
       <Box>teste</Box>
     </DefaultSection>
   );
