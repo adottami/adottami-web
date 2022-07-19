2bb6d8b6c037e4b33a24bef2ab75348c29da1004
AmauriLima <amauri.plimaj@gmail.com>
2022-07-18 22:15:33 -0300

    fix: re-add initial test

--- src/components/pages/account/settings/account-settings-page/__tests__/account-settings-page.test.tsx
+++ src/components/pages/account/settings/account-settings-page/__tests__/account-settings-page.test.tsx
@@ -1,7 +1,21 @@
-export {};
+import { render } from '@testing-library/react';
+
+import { expectPageTitleWithApplicationName } from '@/components/common/page/__tests__/utils';
+
+import AccountSettingsPage from '../account-settings-page';
+import { PAGE_TITLE } from '../constants';
+
+jest.mock('next/router', () => ({
+  useRouter() {
+    return {
+      pathname: '/account/settings',
+    };
+  },
+}));
 
 describe('Account settings page', () => {
   it('should render correctly', () => {
-    // expectPageTitleWithApplicationName(PAGE_TITLE);
+    render(<AccountSettingsPage />);
+    expectPageTitleWithApplicationName(PAGE_TITLE);
   });
 });
