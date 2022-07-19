0673db3bd98b5bbe62dad49694b4da616272bd3e
felipe1496 <felipe.oliveira1496@gmail.com>
2022-07-19 14:42:06 -0300

    fix: adding key to map

--- src/components/pages/publications/search/components/publication-list.tsx
+++ src/components/pages/publications/search/components/publication-list.tsx
@@ -6,13 +6,12 @@ import Publication from '@/models/publication/publication';
 interface Props {
   publications?: Publication[];
 }
-
 const PublicationList: FC<Props> = ({ publications }) => {
   return (
     <div className="flex w-full flex-col items-center gap-6 md:grid md:grid-cols-3">
       {publications &&
         publications.map((publication) => {
-          return <PublicationCard publication={publication} />;
+          return <PublicationCard key={publication.id()} publication={publication} />;
         })}
     </div>
   );
