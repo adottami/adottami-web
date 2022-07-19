63da4ad63a0409b6a3eb068ac8d9f88f7c1d505e
AmauriLima <amauri.plimaj@gmail.com>
2022-07-19 15:56:39 -0300

    fix: select test describe

--- src/components/common/select/__tests__/select.test.tsx
+++ src/components/common/select/__tests__/select.test.tsx
@@ -2,7 +2,7 @@ import { render } from '@testing-library/react';
 
 import Select from '../select';
 
-describe('Radio Group', () => {
+describe('Select', () => {
   it('should have radio input disabled correctly', () => {
     render(<Select />);
   });
