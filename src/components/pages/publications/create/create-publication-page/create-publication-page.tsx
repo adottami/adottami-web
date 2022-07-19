42f61184f343f1d7414977c69b4d8f80797b7014
AmauriLima <amauri.plimaj@gmail.com>
2022-07-19 19:28:18 -0300

    fix

--- src/components/pages/publications/create/create-publication-page/create-publication-page.tsx
+++ src/components/pages/publications/create/create-publication-page/create-publication-page.tsx
@@ -16,7 +16,7 @@ const CreatePublicationPage: FC = () => {
     <Page title={PAGE_TITLE}>
       <FormPageHeader />
 
-      <PublicationForm type="create" header="Qual pet você está anunciando?" onSubmit={handleSubmit} />
+      <PublicationForm type="create" title="Qual pet você está anunciando?" onSubmit={handleSubmit} />
     </Page>
   );
 };
