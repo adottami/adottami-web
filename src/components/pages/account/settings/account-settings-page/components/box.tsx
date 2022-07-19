8332b77894c14b0cf787ac628fdc95912231eb7e
AmauriLima <amauri.plimaj@gmail.com>
2022-07-18 22:04:49 -0300

    fix: changes border radius

--- src/components/pages/account/settings/account-settings-page/components/box.tsx
+++ src/components/pages/account/settings/account-settings-page/components/box.tsx
@@ -8,7 +8,7 @@ const Box: FCC<Props> = (props) => {
   const { children, title } = props;
 
   return (
-    <div className="rounded-3 bg-white p-6 drop-shadow">
+    <div className="rounded-pill bg-white p-6 drop-shadow">
       {title && <h3 className="mb-4 text-lg font-bold text-neutral-800">{title}</h3>}
       {children}
     </div>
