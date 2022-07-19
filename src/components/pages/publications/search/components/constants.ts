8a588abdc8709b014ead17efd3680d3a7f2a1033
felipe1496 <felipe.oliveira1496@gmail.com>
2022-07-19 14:36:25 -0300

    refactor: refactor on constants

---
+++ src/components/pages/publications/search/components/constants.ts
@@ -0,0 +1,34 @@
+export const categoriesInfo = [
+  {
+    variant: 'dog',
+    name: 'Cachorro',
+  },
+  {
+    variant: 'cat',
+    name: 'Gato',
+  },
+  {
+    variant: 'rabbit',
+    name: 'Coelho',
+  },
+  {
+    variant: 'hamster',
+    name: 'Hamster',
+  },
+  {
+    variant: 'bird',
+    name: 'Passaro',
+  },
+  {
+    variant: 'fish',
+    name: 'Peixe',
+  },
+  {
+    name: 'Outros',
+  },
+];
+
+export const LOCATION_INITIAL_STATE = {
+  state: '',
+  city: '',
+};
