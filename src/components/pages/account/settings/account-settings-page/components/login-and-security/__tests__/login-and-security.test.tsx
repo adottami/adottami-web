5a35ea9303e882ae04e27e56af630cecd43c4d82
AmauriLima <amauri.plimaj@gmail.com>
2022-07-20 20:01:06 -0300

    tests: adds login and security section tests

--- src/components/pages/account/settings/account-settings-page/components/login-and-security/__tests__/login-and-security.test.tsx
+++ src/components/pages/account/settings/account-settings-page/components/login-and-security/__tests__/login-and-security.test.tsx
@@ -1,9 +1,25 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
+import { FIELD_LABELS, SECTION_TITLE } from '../constants';
 import LoginAndSecurity from '../login-and-security';
 
 describe('Account settings page', () => {
   it('should render correctly', () => {
     render(<LoginAndSecurity />);
+  });
+
+  it('should render section with correctly title', () => {
+    render(<LoginAndSecurity />);
+    expect(screen.getByText(SECTION_TITLE)).toBeInTheDocument();
+  });
+
+  it('should render section with all fields labels correctly', () => {
+    render(<LoginAndSecurity />);
+    FIELD_LABELS.forEach((fieldLabel) => expect(screen.getByRole('label', { name: fieldLabel })).toBeInTheDocument());
+  });
+
+  it('should render section with correctly button', () => {
+    render(<LoginAndSecurity />);
+    expect(screen.getByRole('button', { name: 'Enviar' })).toBeInTheDocument();
   });
 });
