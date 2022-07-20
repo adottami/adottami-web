2c657719c4865ffa4377c490700720596d471e88
AmauriLima <amauri.plimaj@gmail.com>
2022-07-20 19:58:06 -0300

    feat: adds login and security section layout

---
+++ src/components/pages/account/settings/account-settings-page/components/login-and-security/constants.ts
@@ -0,0 +1,5 @@
+export const SECTION_TITLE = 'Login e segurança';
+
+export const DATA_TESTID = 'login-and-security-test-id';
+
+export const FIELD_LABELS = ['Senha atual *', 'Nova senha *', 'Confirmar nova senha *'];
