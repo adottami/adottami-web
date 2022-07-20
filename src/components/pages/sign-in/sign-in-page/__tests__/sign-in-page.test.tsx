f6cb58a5c610c8d1f27bb7bb1e525cb43e0d1600
Lucas Araújo de Lima <lucas.araujo.lima@ccc.ufcg.edu.br>
2022-07-20 00:03:36 -0300

    fix: change snapshot test to unit test

--- src/components/pages/sign-in/sign-in-page/__tests__/sign-in-page.test.tsx
+++ src/components/pages/sign-in/sign-in-page/__tests__/sign-in-page.test.tsx
@@ -1,14 +1,20 @@
+import { screen } from '@testing-library/dom';
+
 import { expectPageTitleWithApplicationName } from '@/components/common/page/__tests__/utils';
 import { renderWithTestProviders } from '@tests/utils/render';
 
-import { PAGE_TITLE } from '../constants';
+import { PAGE_TITLE, singInPageTestIds } from '../constants';
 import SignInPage from '../sign-in-page';
 
 describe('Sign in page', () => {
   it('should render correctly', () => {
-    const tree = renderWithTestProviders(<SignInPage />);
+    renderWithTestProviders(<SignInPage />);
     expectPageTitleWithApplicationName(PAGE_TITLE);
-
-    expect(tree).toMatchSnapshot();
+    expect(screen.getByTestId(singInPageTestIds.title())).toBeInTheDocument();
+    expect(screen.getByTestId(singInPageTestIds.emailInput())).toBeInTheDocument();
+    expect(screen.getByTestId(singInPageTestIds.passwordInput())).toBeInTheDocument();
+    expect(screen.getByTestId(singInPageTestIds.decorativeImage())).toBeInTheDocument();
+    expect(screen.getByTestId(singInPageTestIds.button())).toBeInTheDocument();
+    expect(screen.getByText(/Cadastre-se/i)).toBeInTheDocument();
   });
 });
