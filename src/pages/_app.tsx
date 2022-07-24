7b64249230b1784c9274caca1e838144a44689c2
Emanuel Moura <emanuel.moura@ccc.ufcg.edu.br>
2022-07-24 17:09:08 -0300

    refactor: changing order of the menu context provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,12 @@ import { FC, useRef } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ToastContainer } from 'react-toastify';
 
+import MenuContextProvider from '@/contexts/account-menu-context/account-menu-context-provider';
 import APIContextProvider from '@/contexts/api-context/api-context-provider';
 import SessionContextProvider from '@/contexts/session-context/session-context-provider';
 
 import '@/styles/global.css';
 import 'react-toastify/dist/ReactToastify.min.css';
-import MenuContextProvider from '@/contexts/account-menu-context/account-menu-context-provider';
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
   const queryClient = useRef(new QueryClient()).current;
