4fae1fabae566a33f1d5015a03a4083550873edb
Emanuel Moura <emanuel.moura@ccc.ufcg.edu.br>
2022-07-25 17:57:23 -0300

    refactor: describing test properly

--- src/components/pages/components/empty-data/__tests__/empty-data.test.tsx
+++ src/components/pages/components/empty-data/__tests__/empty-data.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import EmptyData from '../empty-data';
 
 describe('EmptyData', () => {
-  it('should render footer correctly', () => {
+  it('should render empty data correctly', () => {
     render(<EmptyData message="Aqui está uma messagem !" />);
 
     expect(screen.getByText('Aqui está uma messagem !')).toBeInTheDocument();
