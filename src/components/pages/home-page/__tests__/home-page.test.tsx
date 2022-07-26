158e8c385addd5e90a4422d3423bc2aa679190b0
Emanuel Moura <emanuel.moura@ccc.ufcg.edu.br>
2022-07-26 15:14:40 -0300

    fix(test): adding response handler and using properly the adottamiClient to test

--- src/components/pages/home-page/__tests__/home-page.test.tsx
+++ src/components/pages/home-page/__tests__/home-page.test.tsx
@@ -1,6 +1,15 @@
-import { screen } from '@testing-library/react';
+import { act, screen, waitFor } from '@testing-library/react';
+import axios from 'axios';
 
 import { expectPageTitleWithApplicationName } from '@/components/common/page/__tests__/utils';
+import globalConfig from '@/config/global-config/global-config';
+import useAPI from '@/hooks/api/use-api/use-api';
+import PublicationFactory from '@/models/publication/publication-factory';
+import { PublicationResponse } from '@/models/publication/types';
+import AdottamiClient from '@/services/adottami-client/adottami-client';
+import publicationResponseHandler from '@/services/adottami-client/publication-client/__tests__/mocks/publication-response-handler';
+import PublicationClient from '@/services/adottami-client/publication-client/publication-client';
+import { GetPublicationsOptions } from '@/services/adottami-client/publication-client/types';
 import { renderWithTestProviders } from '@tests/utils/render';
 
 import {
@@ -14,6 +23,89 @@ import {
 } from '../constants';
 import HomePage from '../home-page';
 
+const publicationResponses: PublicationResponse[] = [
+  {
+    id: '1',
+    name: 'Leleco',
+    description: 'My pet',
+    category: 'Cat',
+    gender: 'Male',
+    breed: null,
+    weightInGrams: null,
+    ageInYears: null,
+    zipCode: '00000000',
+    city: 'City',
+    state: 'State',
+    isArchived: false,
+    characteristics: [],
+    images: [
+      {
+        id: '65c3dcbe-2690-437b-bb96-e8487e123284',
+        url: 'C:\\Users\\emanu\\OneDrive\\Documentos\\Engenharia de Software\\Engenharia de Software\\adottami-backend\\tmp\\storage\\65c3dcbe-2690-437b-bb96-e8487e123284',
+      },
+    ],
+    author: {
+      id: '1',
+      name: 'User',
+      email: 'user@email.com',
+      phoneNumber: '1100001111',
+    },
+  },
+  {
+    id: '2',
+    name: 'Lilica',
+    description: 'My pet',
+    category: 'Cat',
+    gender: 'Male',
+    breed: null,
+    weightInGrams: null,
+    ageInYears: null,
+    zipCode: '00000000',
+    city: 'City',
+    state: 'State',
+    isArchived: false,
+    characteristics: [],
+    images: [
+      {
+        id: '65c3dcbe-2690-437b-bb96-e8487e123284',
+        url: 'C:\\Users\\emanu\\OneDrive\\Documentos\\Engenharia de Software\\Engenharia de Software\\adottami-backend\\tmp\\storage\\65c3dcbe-2690-437b-bb96-e8487e123284',
+      },
+    ],
+    author: {
+      id: '1',
+      name: 'User',
+      email: 'user@email.com',
+      phoneNumber: '1100001111',
+    },
+  },
+  {
+    id: '3',
+    name: 'Nunu',
+    description: 'My pet',
+    category: 'Cat',
+    gender: 'Male',
+    breed: null,
+    weightInGrams: null,
+    ageInYears: null,
+    zipCode: '00000000',
+    city: 'City',
+    state: 'State',
+    isArchived: false,
+    characteristics: [],
+    images: [
+      {
+        id: '65c3dcbe-2690-437b-bb96-e8487e123284',
+        url: 'C:\\Users\\emanu\\OneDrive\\Documentos\\Engenharia de Software\\Engenharia de Software\\adottami-backend\\tmp\\storage\\65c3dcbe-2690-437b-bb96-e8487e123284',
+      },
+    ],
+    author: {
+      id: '1',
+      name: 'User',
+      email: 'user@email.com',
+      phoneNumber: '1100001111',
+    },
+  },
+];
 jest.mock('next/router', () => ({
   useRouter() {
     return {
@@ -23,7 +115,22 @@ jest.mock('next/router', () => ({
 }));
 
 describe('Home page', () => {
-  it('should render the first section correctly', () => {
+  const adottamiClient = new AdottamiClient(null);
+
+  it('should render the first section correctly', async () => {
+    const getOptions: GetPublicationsOptions = {
+      city: 'City',
+      state: 'State',
+      categories: ['Category 1', 'Category 2'],
+      isArchived: false,
+      authorId: '1',
+      page: 2,
+      perPage: 30,
+      orderBy: 'createdAt',
+    };
+
+    publicationResponseHandler.mockGet(publicationResponses);
+    await adottamiClient.publications.get(getOptions);
     renderWithTestProviders(<HomePage />);
     expectPageTitleWithApplicationName(PAGE_TITLE);
     expect(screen.getByText(SLOGAN_MESSAGE)).toBeInTheDocument();
@@ -33,7 +140,20 @@ describe('Home page', () => {
     expect(screen.getByText(/saiba mais/i)).toBeInTheDocument();
   });
 
-  it('should render `how adottami works` section correctly', () => {
+  it('should render `how adottami works` section correctly', async () => {
+    const getOptions: GetPublicationsOptions = {
+      city: 'City',
+      state: 'State',
+      categories: ['Category 1', 'Category 2'],
+      isArchived: false,
+      authorId: '1',
+      page: 2,
+      perPage: 30,
+      orderBy: 'createdAt',
+    };
+
+    publicationResponseHandler.mockGet(publicationResponses);
+    await adottamiClient.publications.get(getOptions);
     renderWithTestProviders(<HomePage />);
     expect(screen.getByText(HOW_ADOTTAMI_WORKS)).toBeInTheDocument();
     expect(screen.getByText(FIRST_DESCRIPTION_OF_HOW_IT_WORKS)).toBeInTheDocument();
@@ -42,8 +162,25 @@ describe('Home page', () => {
     expect(screen.getByAltText(/entenda como o adottami funciona/i)).toBeInTheDocument();
   });
 
-  it('should render `recent publications` section correctly', () => {
+  it('should render `recent publications` section correctly', async () => {
+    const getOptions: GetPublicationsOptions = {
+      city: 'City',
+      state: 'State',
+      categories: ['Category 1', 'Category 2'],
+      isArchived: false,
+      authorId: '1',
+      page: 2,
+      perPage: 30,
+      orderBy: 'createdAt',
+    };
+
+    publicationResponseHandler.mockGet(publicationResponses);
+    await adottamiClient.publications.get(getOptions);
     renderWithTestProviders(<HomePage />);
+
+    expect(await screen.findByText('Leleco')).toBeInTheDocument();
+    expect(await screen.findByText('Lilica')).toBeInTheDocument();
+    expect(await screen.findByText('Nunu')).toBeInTheDocument();
     expect(screen.getByText('Anúncios recentes')).toBeInTheDocument();
     expect(screen.getByText('Ver mais')).toBeInTheDocument();
     expect(screen.getByTestId('cards')).toBeInTheDocument();
