6c416521ae6fe1fa58cabc48969c08f3edd2f44e
Emanuel Moura <emanuel.moura@ccc.ufcg.edu.br>
2022-07-27 20:57:45 -0300

    test: simple render test

--- src/components/pages/publications/dashboard/publication-dashboard-page/__tests__/publication-dashboard-page.test.tsx
+++ src/components/pages/publications/dashboard/publication-dashboard-page/__tests__/publication-dashboard-page.test.tsx
@@ -1,13 +1,77 @@
-import { render } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 
 import { expectPageTitleWithApplicationName } from '@/components/common/page/__tests__/utils';
+import createPublication from '@/models/publication/__tests__/factories/publication-factory';
+import PublicationFactory from '@/models/publication/publication-factory';
+import publicationResponseHandler from '@/services/adottami-client/publication-client/__tests__/mocks/publication-response-handler';
+import { renderWithTestProviders } from '@tests/utils/render';
 
 import { PAGE_TITLE } from '../constants';
 import PublicationDashboardPage from '../publication-dashboard-page';
 
+jest.mock('next/router', () => ({
+  useRouter() {
+    return {
+      pathname: 'publications/dashboard',
+    };
+  },
+}));
 describe('Publication dashboard page', () => {
-  it('should render correctly', () => {
-    render(<PublicationDashboardPage />);
+  const publications = [
+    createPublication({
+      name: 'Leleco',
+      images: [
+        {
+          id: '1',
+          url: 'imagem.png',
+        },
+      ],
+    }),
+    createPublication({
+      name: 'Lilica',
+      images: [
+        {
+          id: '1',
+          url: 'imagem.png',
+        },
+      ],
+    }),
+    createPublication({
+      name: 'Nunu',
+      images: [
+        {
+          id: '1',
+          url: 'imagem.png',
+        },
+      ],
+    }),
+    createPublication({
+      name: 'Augusto',
+      images: [
+        {
+          id: '1',
+          url: 'imagem.png',
+        },
+      ],
+    }),
+  ];
+  const publicationResponses = publications.map((publication) => PublicationFactory.toResponse(publication));
+
+  beforeEach(() => {
+    publicationResponseHandler.mockGet(publicationResponses);
+  });
+
+  it('should render correctly', async () => {
+    renderWithTestProviders(<PublicationDashboardPage />);
+    const myAds = await screen.findAllByText('Meus anúncios');
+
     expectPageTitleWithApplicationName(PAGE_TITLE);
+    expect(await screen.findByText('Leleco')).toBeInTheDocument();
+    expect(await screen.findByText('Lilica')).toBeInTheDocument();
+    expect(await screen.findByText('Nunu')).toBeInTheDocument();
+    expect(await screen.findByText('Augusto')).toBeInTheDocument();
+    expect(screen.getByText('Publicados (4)')).toBeInTheDocument();
+
+    expect(myAds.length).toEqual(2);
   });
 });
