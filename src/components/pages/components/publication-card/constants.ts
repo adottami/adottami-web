eacae775c4baef5bbd99bff3fb2e9e870b2b9f11
Emanuel Moura <emanuel.moura@ccc.ufcg.edu.br>
2022-07-28 13:41:07 -0300

    feat: adding toast config

--- src/components/pages/components/publication-card/constants.ts
+++ src/components/pages/components/publication-card/constants.ts
@@ -1,1 +1,9 @@
 export const LINK_TO_DETAILS_PAGE_LABEL = 'Ver detalhes da publicação';
+
+export const TOAST_CONFIGS = {
+  autoClose: 3000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+};
