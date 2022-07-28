1cf8d6cfbd8bdb8a66bfc95ffb2d047e4a4b78cf
Emanuel Moura <emanuel.moura@ccc.ufcg.edu.br>
2022-07-28 13:40:34 -0300

    feat: adding publicationId to handle click at CardMenu

--- src/components/pages/components/publication-card/components/card-subheader.tsx
+++ src/components/pages/components/publication-card/components/card-subheader.tsx
@@ -3,17 +3,18 @@ import { FC } from 'react';
 import CardMenu from './card-menu';
 
 interface Props {
+  publicationId: string;
   name: string;
   isVisible?: boolean;
 }
 
 const CardSubheader: FC<Props> = (props) => {
-  const { name, isVisible } = props;
+  const { name, isVisible, publicationId } = props;
 
   return (
     <div className="flex items-center justify-between gap-6">
       <h3 className="text-2xl font-bold">{name}</h3>
-      <CardMenu isVisible={isVisible} />
+      <CardMenu isVisible={isVisible} publicationId={publicationId} />
     </div>
   );
 };
