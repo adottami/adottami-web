a4af0019bca62a3fff030ae4622e4d107aa71d14
Diego Aquino <diegocruzdeaquino@gmail.com>
2022-07-31 13:51:15 -0300

    refactor: simplify login and security journey

--- src/components/pages/account/settings/account-settings-page/components/login-and-security/login-and-security.tsx
+++ src/components/pages/account/settings/account-settings-page/components/login-and-security/login-and-security.tsx
@@ -15,16 +15,17 @@ import { DATA_TESTID, SECTION_TITLE } from './constants';
 import { loginAndSecurityFormSchema } from './schemas/login-and-security-form-schema';
 import { ChangePasswordFormData } from './types';
 
+type Step = 1 | 2;
+
 interface Props {
-  testStep?: number;
+  defaultStep?: Step;
 }
 
-const LoginAndSecurity: FC<Props> = (props) => {
-  const { testStep } = props;
+const LoginAndSecurity: FC<Props> = ({ defaultStep = 1 }) => {
   const { user } = useSession();
   const api = useAPI();
 
-  const [step, setStep] = useState<number>(testStep || 1);
+  const [step, setStep] = useState<Step>(defaultStep);
   const [showErrors, setShowErrors] = useState<boolean>(false);
 
   const { values, errors, handleChange, handleSubmit } = useFormik<ChangePasswordFormData>({
@@ -60,28 +61,6 @@ const LoginAndSecurity: FC<Props> = (props) => {
     handleSubmit(event);
   };
 
-  if (step === 1) {
-    return (
-      <DefaultSection
-        data-testid={DATA_TESTID}
-        title={SECTION_TITLE}
-        description="Aumente a segurança e tenha o controle da sua conta"
-      >
-        <AccountSettingsSection
-          title="Alteração de senha"
-          description={[
-            <p key="first line">Escolha uma senha forte que você não esteja usando em nenhum outro lugar.</p>,
-            <p key="second line">Troque sua senha a cada 6 meses para aumentar a segurança da sua conta.</p>,
-          ]}
-        >
-          <div>
-            <Button onClick={() => setStep(2)}>Alterar senha</Button>
-          </div>
-        </AccountSettingsSection>
-      </DefaultSection>
-    );
-  }
-
   return (
     <DefaultSection
       data-testid={DATA_TESTID}
@@ -95,47 +74,55 @@ const LoginAndSecurity: FC<Props> = (props) => {
           <p key="second line">Troque sua senha a cada 6 meses para aumentar a segurança da sua conta.</p>,
         ]}
       >
-        <form className="flex flex-col gap-4" onSubmit={onHandleSubmit} noValidate>
-          <Input
-            label="Senha atual"
-            placeholder="Digite sua senha atual"
-            variant="password"
-            isRequired
-            name="currentPassword"
-            id="currentPassword"
-            value={values.currentPassword}
-            errorMessage={showErrors ? errors.currentPassword : ''}
-            onChange={handleChange}
-          />
-          <Input
-            label="Nova senha"
-            placeholder="Digite sua nova senha"
-            variant="password"
-            description="6 ou mais caracteres"
-            isRequired
-            name="newPassword"
-            id="newPassword"
-            value={values.newPassword}
-            errorMessage={showErrors ? errors.newPassword : ''}
-            onChange={handleChange}
-          />
-          <Input
-            label="Confirmar nova senha"
-            placeholder="Digite sua nova senha novamente"
-            variant="password"
-            description="6 ou mais caracteres"
-            isRequired
-            name="confirmNewPassword"
-            id="confirmNewPassword"
-            value={values.confirmNewPassword}
-            errorMessage={showErrors ? errors.confirmNewPassword : ''}
-            onChange={handleChange}
-          />
-
-          <div className="mt-4">
-            <Button type="submit">Enviar</Button>
+        {step === 1 && (
+          <div>
+            <Button onClick={() => setStep(2)}>Alterar senha</Button>
           </div>
-        </form>
+        )}
+
+        {step === 2 && (
+          <form className="flex flex-col gap-4" onSubmit={onHandleSubmit} noValidate>
+            <Input
+              label="Senha atual"
+              placeholder="Digite sua senha atual"
+              variant="password"
+              isRequired
+              name="currentPassword"
+              id="currentPassword"
+              value={values.currentPassword}
+              errorMessage={showErrors ? errors.currentPassword : ''}
+              onChange={handleChange}
+            />
+            <Input
+              label="Nova senha"
+              placeholder="Digite sua nova senha"
+              variant="password"
+              description="6 ou mais caracteres"
+              isRequired
+              name="newPassword"
+              id="newPassword"
+              value={values.newPassword}
+              errorMessage={showErrors ? errors.newPassword : ''}
+              onChange={handleChange}
+            />
+            <Input
+              label="Confirmar nova senha"
+              placeholder="Digite sua nova senha novamente"
+              variant="password"
+              description="6 ou mais caracteres"
+              isRequired
+              name="confirmNewPassword"
+              id="confirmNewPassword"
+              value={values.confirmNewPassword}
+              errorMessage={showErrors ? errors.confirmNewPassword : ''}
+              onChange={handleChange}
+            />
+
+            <div className="mt-4">
+              <Button type="submit">Enviar</Button>
+            </div>
+          </form>
+        )}
       </AccountSettingsSection>
     </DefaultSection>
   );
